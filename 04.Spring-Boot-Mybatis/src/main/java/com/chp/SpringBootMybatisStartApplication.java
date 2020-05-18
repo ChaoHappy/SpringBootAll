1f20a0248c27e654596975fe558df771bed86d84
ChaoHappy <871761665@qq.com>
2020-05-18 23:13:16 +0800

    Spring-Boot-Mybatis学习笔记

---
+++ 04.Spring-Boot-Mybatis/src/main/java/com/chp/SpringBootMybatisStartApplication.java
@@ -0,0 +1,13 @@
+package com.chp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisStartApplication.class, args);
+    }
+
+}
---
+++ 04.Spring-Boot-Mybatis/src/test/java/com/chp/SpringBootMybatisStartApplicationTests.java
@@ -0,0 +1,14 @@
+package com.chp;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
